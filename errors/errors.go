353147d29be010c0527223ed75d0c50973f7d319
Nathan Hemingway <nhemingway@gmail.com>
2026-01-26 17:11:33 -0800

    re-export errors Is and As functions

--- errors/errors.go
+++ errors/errors.go
@@ -5,9 +5,16 @@ import (
 	"github.com/cockroachdb/errors"
 )
 
+// Error creation functions
 var (
 	WithStack = errors.WithStack
 	Wrapf     = errors.Wrapf
 	New       = errors.New
 	Newf      = errors.Newf
+)
+
+// Error inspection functions
+var (
+	Is = errors.Is
+	As = errors.As
 )
